f8890beac262d4e88135b1b2d7962c7205cb4302
Unimke1990 <agimunimke1990@gmail.com>
2024-10-16 16:15:53 +0200

    function that returns time delayed

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,8 +1,16 @@
+#!/urs/bin/env python3
+"""
+importing need packages
+"""
 import asyncio
+import random
 
-async def myfucnt():
-        print("starting my function")
-        await asyncio.sleep(3)
-        print("continuing my work after waiting")
 
-        asyncio.run(myfucnt())
+"""function that returns time delayed"""
+async def wait_random(max_delay: int = 10) -> float:
+    waitTime = random.uniform(0, max_delay)
+    await asyncio.sleep(waitTime)
+    print(waitTime)
+    return waitTime   
+asyncio.run(wait_random())   
+    
