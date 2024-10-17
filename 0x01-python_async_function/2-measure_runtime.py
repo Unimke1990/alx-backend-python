1269d72558acb0e87f10cef92b52b27ad6698018
Unimke1990 <agimunimke1990@gmail.com>
2024-10-17 13:50:27 +0200

    function that calculates the time it took to run another function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
-"""module that defines the measure_time function"""
-import random
+"""This module defines the function `measure_time`"""
 import asyncio
+import random
 import time
-
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """measures the total execution time for `wait_n(n, max_delay)`"""
-    start_time = time.time()
-    await wait_n(n, max_delay)
-    end_time = time.time()
+    startTime = time.time()
 
-    total_time = end_time - start_time
-    return total_time / n
+    asyncio.run(wait_n(n, max_delay))
+
+    endTime = time.time()
+    totalTime = endTime - startTime
+
+    return totalTime / n
